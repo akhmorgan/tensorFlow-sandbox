6c49620153faec9b387e581626c2d6f1a6ec8468
Adam Morgan <akhmorgan@hotmail.com>
2018-10-16 16:34:06 +0100

    Initial updates to with_keras version

--- with_keras.py
+++ with_keras.py
@@ -9,8 +9,27 @@ import matplotlib.pyplot as plt
 print(tf.__version__)
 
 # load data
+data = pd.read_csv("./zoo-animal-classification/zoo.csv")
+
+# shuffle and split the dataset
+data = data.sample(frac=1).reset_index(drop=True)
+data_total_len = data[data.columns[0]].size
+data_train_frac = 0.6
+split_index = math.floor(data_total_len*data_train_frac)
+training_data = data.iloc[:split_index]
+evaluation_data = data.iloc[split_index:];
 
 # preprocess data
+def preprocess(data):
+    features = data.iloc[:, 1:17] #all rows, all the features and no labels
+    labels= data.iloc[:, 17] #all rows, label only
+    labels = labels-1 # shift value range from 1-7 to be 0-6
+    return features, labels
+
+animal_type=['Mammal', 'Bird', 'Reptile', 'Fish', 'Amphibian', 'Bug', 'Invertebrate']
+
+(train_data, train_labels) = preprocess(training_data);
+print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)))
 
 # build the model
 model = keras.Sequential()
